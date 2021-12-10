4734c19753b99607e766cee3c9a8fd70eea93e91
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-12-10 13:41:30 +0530

    directory component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+// this one needs to be a class compenet And this one will need to be a class component because we do need to store the state value of those menu items that we want to pass and create our menu items with. Right.So just like when we did our monster's Rolodex and we had that card list component, this one is going to have a directory with a state value of those menu items.
+// const DirectoryList = () => (
+
+// )
+
+// export default DirectoryList;
+
+
+
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+himport React from "react";
 
-const MenuList = () => {
+//We probably want to be able to dynamically generate title by passing a title into our menu component right as a props.So instead of doing something like MenuList = (props) => then <h1 className='title'> props.title</h1> props.title like this, we might also need some other properties to be passed in. If we look at our application, we're going to see that each one of these has a different. Image, right? So what we can do is we can actually structure the title off of our props for now and then we can just populate our component with that destruction value of title from our props. This is the exact same thing as writing props, that title, except we're pulling that title value off of our props and then setting it to this title value inside of our function so we can use it whenever we want.And then let's export out our menu item.And now we have our menu items.
+const MenuItem = ({title}) => {
   <div className="menu-item">
     <div className="content">
-      <h1 className="title">HATS</h1>
+      <h1 className="title">{titel}</h1>
       <span className="subtitle">SHOP</span>
     </div>
   </div>;
 };
 
-export default MenuList;
+export default MenuItem;
