b91693406fa1a876a6abb3581e0825edc1a2b2c1
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-12-27 19:30:02 +0530

    installed react-dom-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-// import HomePage from "./pages/homepage/homepage.component";
-
-// function App() {
-//   return (
-//     <div>
-//       <HomePage></HomePage>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import "./App.css";
 
